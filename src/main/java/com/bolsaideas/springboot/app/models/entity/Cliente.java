a01e2bcd911cc47f6e5e13db74b9eb13beb71c87
Hipolito Tapia PC <u20171a057@upc.edu.pe>
2022-08-12 15:43:21 -0500

    cambios

--- src/main/java/com/bolsaideas/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/bolsaideas/springboot/app/models/entity/Cliente.java
@@ -21,7 +21,7 @@ public class Cliente implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	@Column(name = "nombre")
 	private String nombre;
 	private String apellido;
 	private String email;
